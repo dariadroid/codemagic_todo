e8742f9949ae631d0562c0f529885c9c7ae39e37
daria <darja.tretjakova@chi.lv>
2022-02-10 19:22:08 +0200

    - Add failing unit test

---
+++ test/calculator_test.dart
@@ -0,0 +1,16 @@
+import 'package:codemagic_todo/calculator.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  late Calculator calculator;
+  setUp(() {
+    calculator = Calculator();
+  });
+
+  group('calculator tests', () {
+    test('correctly counts sum', () {
+      final sum = calculator.sum(2, 2);
+      expect(sum, 3);
+    });
+  });
+}
